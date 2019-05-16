57e65ab7536ad99e2badbf0baa32e8136b6a6110
woghd9072 <woghd9072@gmail.com>
2019-05-16 20:22:41 +0900

    태초마을

--- Project-TDL/src/main/java/com/jaehong/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jaehong.repository;
-
-import com.jaehong.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findById(String id);
-    User findByEmail(String email);
-}
