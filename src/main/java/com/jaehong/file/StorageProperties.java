9f7b5467fbd772ae4f2ca37fdea2dd8636a6c352
woghd9072 <woghd9072@gmail.com>
2019-07-11 20:02:39 +0900

    성공

---
+++ src/main/java/com/jaehong/file/StorageProperties.java
@@ -0,0 +1,18 @@
+package com.jaehong.file;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("file")
+public class StorageProperties {
+
+    private String location = "upload-dir";
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+}
